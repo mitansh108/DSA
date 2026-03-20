b4c3baea3d43249e6d9b18b641aa9aaa3c7a78ad
Mitansh Patel <mitanshpatel10@gmail.com>
2026-03-20 10:43:56 -0700

    update

---
+++ insertionsort.py
@@ -0,0 +1,10 @@
+class Solution:
+    def insertionSort(self, nums):
+        n = len(nums)
+        for i in range(0,n):
+            j = i
+            while j > 0 and nums[j-1] > nums[j]:
+                nums[j], nums[j-1] = nums[j-1], nums[j]
+                j-=1
+
+        return nums
