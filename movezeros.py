2de8f7bf3f0e7df0fdc7f7cdce7ebb478d25cdaf
Mitansh Patel <mitanshpatel10@gmail.com>
2026-03-22 15:02:53 -0700

    update

---
+++ movezeros.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        
+        """
+        n = len(nums)
+        l = 0
+        for r in range(n):
+            if nums[r] != 0:
+                nums[r], nums[l] = nums[l], nums[r]
+                l+=1
