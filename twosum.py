c13aa9ceabd3117b5164b469ca52bb5a2892ed2d
Mitansh Patel <mitanshpatel10@gmail.com>
2026-03-24 14:36:55 -0700

    update

---
+++ twosum.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+
+        """
+        n = len(nums)
+        ans = {}
+
+        for i in range (n):
+            val = nums[i]
+            partner = target - val
+
+            if partner in ans:
+                return[ans[partner], i]
+            
+            else:
+                ans[val] = i
+
+    
+        
+
+        
+            
+
+        
+        
