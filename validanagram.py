06d3ba7dde73b058201b97c95c1d13d6635304d4
Mitansh Patel <mitanshpatel10@gmail.com>
2026-03-25 07:17:19 -0700

    update

---
+++ validanagram.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        
+        countS = {}
+        countT = {}
+        
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        for x in countS:
+            if countS[x] != countT.get(x,0):
+                return False
+        return True
+
