47e67575ac759929583d44ef7ff85ffcb278ef81
Mitansh Patel <mitanshpatel10@gmail.com>
2026-03-27 15:49:52 -0700

    update

---
+++ topkfrequent.py
@@ -0,0 +1,18 @@
+import heapq
+from collections import Counter
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        counter = Counter(nums)
+        heap = []
+        
+        for key, val in counter.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (val,key))
+            else:
+                heapq.heappushpop(heap, (val,key))
+        return [h[1] for h in heap]
