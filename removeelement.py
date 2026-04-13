6b50a4a40097e10daf6e70b1b0d7b3395593386d
Mitansh Patel <mitanshpatel10@gmail.com>
2026-04-13 11:16:25 -0700

    update

---
+++ removeelement.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        r = 0
+
+        for l in range(len(nums)):
+            if nums[l] != val:
+                nums[r] = nums[l]
+                r+=1
+        
+        return r
